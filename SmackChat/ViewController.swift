2ab0780be93b1e9fcdeea9d802c82d3019bd6e2f
Hammoud Hammoud <hammoudhammoud@TJs-Mac-mini.local>
2017-12-04 13:56:51 -0800

    Initial Commit

---
+++ SmackChat/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SmackChat
+//
+//  Created by Hammoud Hammoud on 12/4/17.
+//  Copyright © 2017 Hammoud Hammoud. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
